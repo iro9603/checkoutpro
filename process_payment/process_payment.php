83dbe501a9dac20a3ef6f9fc05216b9bcd0cad5d
Irving <riosirving04@gmail.com>
2021-09-08 13:51:38 -0500

    version 1

---
+++ process_payment/process_payment.php
@@ -0,0 +1,39 @@
+
+    <?php
+    
+    header ("Access-Control-Allow-Origin: *");
+    header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
+    header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    header ("Access-Control-Allow-Headers: *");
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        // SDK de Mercado Pago
+        require_once 'C:\wamp64\www\ejemplo2\vendor\autoload.php';
+        // Agrega credenciales
+        MercadoPago\SDK::setAccessToken('TEST-11762472748563-081923-f6c1d5d1d637d865580d438da9d492c8-448109497');
+        $payment = new MercadoPago\Payment();
+        $payment->transaction_amount = (float)$_POST['transactionAmount'];
+        $payment->token = $_POST['token'];
+        $payment->description = $_POST['description'];
+        $payment->installments = (int)$_POST['installments'];
+        $payment->payment_method_id = $_POST['paymentMethodId'];
+        $payment->issuer_id = (int)$_POST['issuer'];
+        
+        $payer = new MercadoPago\Payer();
+        $payer->email = $_POST['email'];
+        $payer->identification = array(
+            "number" => $_POST['docNumber']
+        );
+        $payment->payer = $payer;
+        
+        $payment->save();
+        
+        $response = array(
+            'status' => $payment->status,
+            'status_detail' => $payment->status_detail,
+            'id' => $payment->id
+        );
+        echo json_encode($response);
+    };
+
+
+?>
