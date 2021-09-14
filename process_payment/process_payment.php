3b1b051a939d8790e5c824fbe9e6835e284fe454
Irving <riosirving04@gmail.com>
2021-09-13 23:05:02 -0500

    actualizacion 11:04pm

--- process_payment/process_payment.php
+++ process_payment/process_payment.php
@@ -14,33 +14,33 @@
 
             case 'https://finanzas-aantik.azurewebsites.net/process_payment/process_payment.php':
                     // SDK de Mercado Pago
-            require __DIR__.'/vendor/autoload.php';
-            // Agrega credenciales
-            MercadoPago\SDK::setAccessToken('TEST-8901633554153246-091404-3076fccec30c2a1db9e6e04c23dd416c-812487226');
-            $payment = new MercadoPago\Payment();
-            $payment->transaction_amount = (float)$_POST['transactionAmount'];
-            $payment->token = $_POST['token'];
-            $payment->description = $_POST['description'];
-            $payment->installments = (int)$_POST['installments'];
-            $payment->payment_method_id = $_POST['paymentMethodId'];
-            $payment->issuer_id = (int)$_POST['issuer'];
-            
-            $payer = new MercadoPago\Payer();
-            $payer->email = $_POST['email'];
-            $payer->identification = array(
-                "number" => $_POST['docNumber']
-            );
-            $payment->payer = $payer;
-            
-            $payment->save();
-            
-            $response = array(
-                'status' => $payment->status,
-                'status_detail' => $payment->status_detail,
-                'id' => $payment->id
-            );
-            echo json_encode($response);
-            break;
+                require __DIR__.'/vendor/autoload.php';
+                // Agrega credenciales
+                MercadoPago\SDK::setAccessToken('TEST-8901633554153246-091404-3076fccec30c2a1db9e6e04c23dd416c-812487226');
+                $payment = new MercadoPago\Payment();
+                $payment->transaction_amount = (float)$_POST['transactionAmount'];
+                $payment->token = $_POST['token'];
+                $payment->description = $_POST['description'];
+                $payment->installments = (int)$_POST['installments'];
+                $payment->payment_method_id = $_POST['paymentMethodId'];
+                $payment->issuer_id = (int)$_POST['issuer'];
+                
+                $payer = new MercadoPago\Payer();
+                $payer->email = $_POST['email'];
+                $payer->identification = array(
+                    "number" => $_POST['docNumber']
+                );
+                $payment->payer = $payer;
+                
+                $payment->save();
+                
+                $response = array(
+                    'status' => $payment->status,
+                    'status_detail' => $payment->status_detail,
+                    'id' => $payment->id
+                );
+                echo json_encode($response);
+                break;
                 
     }
         
